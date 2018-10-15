54485e9e77c330c980063da1792486492a92b146
Prasad Palla <prasad.palla@outlook.com>
2018-10-16 00:26:56 +0530

    This is my First NodeJs Files

---
+++ index.js
@@ -0,0 +1,11 @@
+// var _ = require('underscore');
+
+
+
+// var result = _.contains([1, 2, 3], 3);
+// console.log(result);
+
+
+var _ = require('underscore');
+var result = _.contains([1,2,3],4);
+console.log(result);
